bc749b1418614feea31f65f642ff54a050eed679
Whitehead549 <finbareric549@gmail.com>
2025-01-16 13:28:21 +0100

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { FaSpinner } from "react-icons/fa"; // Import Font Awesome spinner
 import heroBanner from "../assets/2150170352.jpg";
+import image1 from "../assets/Digital_art.jpg";
+import image2 from "../assets/Education.jpg";
+import image3 from "../assets/consultant.jpg";
+import image4 from "../assets/fine_art.jpg";
+import image5 from "../assets/room.jpg";
+import image6 from "../assets/logissss.jpg";
 import Testimonial from "../components/Essentials/Testimonial";
-import ImageGrid from "../components/Essentials/ImageGrid";
 import Frequentqes from "../components/Frequentqes";
 
 const Home = ({ setIsHomeLoading }) => {
@@ -17,6 +22,16 @@ const Home = ({ setIsHomeLoading }) => {
     };
   }, [setIsHomeLoading]);
 
+  // Image Grid Data
+  const images = [
+    { src: image4, title: "FINE ART" },
+    { src: image1, title: "DIGITAL ART" },
+    { src: image2, title: "ART EDUCATION" },
+    { src: image5, title: "ART STORAGE" },
+    { src: image3, title: "ART CONSULTING" },
+    { src: image6, title: "ART LOGISTICS" },
+  ];
+
   return (
     <div className="bg-gray-100 overflow-x-hidden">
       {/* Show placeholder or loader until image is fully loaded */}
@@ -25,7 +40,7 @@ const Home = ({ setIsHomeLoading }) => {
           {/* Hero Section */}
           <div
             id="hero-section"
-            className="relative w-full h-[50vh] sm:h-[80vh] md:h-[70vh] lg:h-[80vh] overflow-hidden  bg-[#000300]"
+            className="relative w-full h-[50vh] sm:h-[80vh] md:h-[70vh] lg:h-[80vh] overflow-hidden bg-[#000300]"
           >
             <div className="absolute inset-0">
               <div className="absolute inset-0 bg-black bg-opacity-55"></div>
@@ -39,7 +54,7 @@ const Home = ({ setIsHomeLoading }) => {
             {/* Content Wrapper */}
             <div className="absolute inset-0 flex flex-col items-center justify-center text-center pt-12 px-4">
               <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-white mb-6 leading-tight max-w-3xl tracking-wide">
-              Creative<br/>Art Work
+                Creative<br />Art Work
               </h1>
 
               <div className="flex flex-col sm:flex-row space-y-0 sm:space-y-0 sm:space-x-6 justify-center pt-0">
@@ -55,7 +70,28 @@ const Home = ({ setIsHomeLoading }) => {
 
           {/* Other Sections */}
           <div id="other-sections">
-            <ImageGrid />
+            {/* Image Grid Section */}
+            <section className="text-[#ffffff] text-center py-4 md:py-6 lg:py-6 bg-[#000300]">
+              <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold">
+                Our Creative Collection
+              </h1>
+            </section>
+
+            <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 py-2 px-12 max-w-6xl mx-auto">
+              {images.map((image, index) => (
+                <div key={index} className="text-center">
+                  <img
+                    src={image.src}
+                    alt={image.title}
+                    className="w-full h-auto object-contain mb-2"
+                  />
+                  <h2 className="text-md font-semibold text-[#000000]">
+                    {image.title}
+                  </h2>
+                </div>
+              ))}
+            </section>
+
             <Frequentqes />
             <Testimonial />
           </div>
