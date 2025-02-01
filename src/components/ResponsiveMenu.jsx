233e00e319cc19605e30bd811602ffd208f06ba8
Whitehead549 <finbareric549@gmail.com>
2025-02-01 20:55:48 +0100

    first commit

--- src/components/ResponsiveMenu.jsx
+++ src/components/ResponsiveMenu.jsx
@@ -30,14 +30,14 @@ const ResponsiveMenu = ({ showMenu, setShowMenu}) => {
     <div
       className={`${
         showMenu ? "left-0" : "-left-[100%]"
-      } fixed bottom-0 top-0 z-20 flex h-[65%] w-[55%] flex-col justify-between bg-[#FAFAFA] px-8 pb-0 pt-4 transition-all duration-200 md:hidden rounded-r-xl shadow-md`}
+      } fixed bottom-0 top-0 z-20 flex h-[63%] w-[50%] flex-col justify-between bg-[#FAFAFA] px-8 pb-0 pt-12 transition-all duration-200 md:hidden rounded-r-xl shadow-md`}
     >
       <div className="Navbar_card">
         {/* Top section */}
        
         {/* Navlinks section */}
-        <div className='text-black mt-10'> {/* Changed to text-black */}
-        <ul className='space-y-2 text-xl'>
+        <div className='text-black mt-14'> {/* Changed to text-black */}
+        <ul className='space-y-1 text-xl'>
           {
             navbarLinks.map(({ name, link, id }) => (
               <li key={id}>
